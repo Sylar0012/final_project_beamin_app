0d53c87c983333f22166ea9bac1f19af375f7795
Sylar <108394995+Sylar0012@users.noreply.github.com>
2022-12-04 01:26:56 +0900

    회원 가입 및 로그인 완성 (#24)
    회원 가입 및 로그인 완성

---
+++ lib/view/pages/components/costom_text_form_field.dart
@@ -0,0 +1,47 @@
+import 'package:final_project_beamin_app/constants.dart';
+import 'package:final_project_beamin_app/size.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({required this.text, Key? key}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            SizedBox(width: gap_xs),
+            Text(text),
+          ],
+        ),
+        SizedBox(height: gap_xxs),
+        TextFormField(
+          validator: (value) => value!.isEmpty ? "${text}를 입력해주세요" : null,
+          obscureText: text == "비밀번호" ? true : false,
+          decoration: InputDecoration(
+            hintText: "${text}를 입력하세요",
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: kMenuIconColor),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: kMenuIconColor),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: kMenuIconColor),
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
