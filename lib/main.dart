0f57b2eb07518aeebe6a6e9c818c7125f80e5c8b
Sylar <kmc42692@naver.com>
2022-12-10 10:45:31 +0900

    router 변수명으로 지정, 통신을 위한 라이브러리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_project_beamin_app/core/routes.dart';
 import 'package:final_project_beamin_app/theme.dart';
 import 'package:final_project_beamin_app/view/pages/main/favorite_store/favorite_store.dart';
 import 'package:final_project_beamin_app/view/pages/main/main_page.dart';
@@ -12,7 +13,6 @@ import 'package:final_project_beamin_app/view/pages/order/order_list/my_order_li
 import 'package:final_project_beamin_app/view/pages/order/payment/payment.dart';
 import 'package:final_project_beamin_app/view/pages/order/payment_detail.dart';
 import 'package:final_project_beamin_app/view/pages/store/menu_detail/menu_detail.dart';
-import 'package:final_project_beamin_app/view/pages/store/store_detail/components/store_detail_header.dart';
 import 'package:final_project_beamin_app/view/pages/store/store_detail/store_detail.dart';
 import 'package:final_project_beamin_app/view/pages/user/join/join_Page.dart';
 import 'package:final_project_beamin_app/view/pages/user/login/login_page.dart';
@@ -29,22 +29,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/login": (context) => LoginPage(),
-        "/join": (context) => JoinPage(),
-        "/home": (context) => MainPage(), // 더미 완
-        "/myBaemin": (context) => MyBaemin(),
-        "/favoriteStore": (context) => FavoriteStore(), // 더미 완
-        "/orderDetail": (context) => MyOrderDetail(),
-        "/storeDetail": (context) => StoreDetail(), // 더미 완
-        "/orderList": (context) => OrderList(),
-        "/myReview": (context) => ReviewList(),
-        "/reviewWrite": (context) => ReviewWrite(),
-        "/inactive": (context) => UserInactive(),
-        "/update": (context) => InfoUpdate(),
-        "/myOrderList": (context) => MyOrderList(id: 0),
-        "/payment": (context) => Payment(),
-        "/paymentDetail": (context) => PaymentDetail(),
-        "/menuDetail": (context) => MenuDetail(), // 더미 완
+        Routers.login: (context) => LoginPage(),
+        Routers.join: (context) => JoinPage(),
+        Routers.home: (context) => MainPage(), // 더미 완
+        Routers.myBaemin: (context) => MyBaemin(),
+        Routers.favoriteStore: (context) => FavoriteStore(), // 더미 완
+        Routers.myOrderList: (context) => MyOrderDetail(),
+        Routers.storeDetail: (context) => StoreDetail(), // 더미 완
+        Routers.orderList: (context) => OrderList(),
+        Routers.reviewList: (context) => ReviewList(),
+        Routers.reviewWrite: (context) => ReviewWrite(),
+        Routers.inactive: (context) => UserInactive(),
+        Routers.infoUpdate: (context) => InfoUpdate(),
+        Routers.myOrderList: (context) => MyOrderList(id: 0),
+        Routers.payment: (context) => Payment(),
+        Routers.paymentDetail: (context) => PaymentDetail(),
+        Routers.menuDetail: (context) => MenuDetail(), // 더미 완
       },
       title: "beamin_ui",
       debugShowCheckedModeBanner: false,
