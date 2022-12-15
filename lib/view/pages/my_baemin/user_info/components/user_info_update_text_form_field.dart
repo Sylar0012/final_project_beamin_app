d5b6c2fe74d84ecf780bf35021717cff0af75730
Sylar <kmc42692@naver.com>
2022-12-16 07:05:56 +0900

    info_update 페이지 _buildPasswordUpdate 구조 수정

---
+++ lib/view/pages/my_baemin/user_info/components/user_info_update_text_form_field.dart
@@ -0,0 +1,42 @@
+import 'package:final_project_beamin_app/constants.dart';
+import 'package:final_project_beamin_app/size.dart';
+import 'package:flutter/material.dart';
+
+class UserInfoUpdateTextFormField extends StatelessWidget {
+  const UserInfoUpdateTextFormField({required this.hintMsg, required this.controller, required this.funValidator, Key? key}) : super(key: key);
+  final String hintMsg;
+  final TextEditingController controller;
+  final funValidator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      textAlign: TextAlign.center,
+      controller: controller,
+      textAlignVertical: TextAlignVertical.bottom,
+      validator: funValidator,
+      obscureText: hintMsg == "비밀번호" ? true : false,
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(vertical: gap_xs),
+        hintText: "${hintMsg}를(을) 입력하세요",
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: kMenuIconColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: kMenuIconColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: kMenuIconColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
